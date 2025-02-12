2a2c0689375fd2811f1fc2b8eb0684f45b4b88a6
Sumbal Naz <Codeq209@gmail.com>
2025-02-12 23:59:22 +0500

    Add files via upload

---
+++ Step03_Datatype/index.py
@@ -0,0 +1,35 @@
+# int (integer)
+age = 25
+print("Integer:", age) 
+
+# float (decimal)
+pi = 3.14
+print("Float:", pi)  
+
+# str (string)
+name = "Code Queen"
+print("String:", name) 
+
+# bool (boolean)
+is_active = True
+print("Boolean:", is_active)  
+
+# list
+fruits = ["apple", "banana", "mango"]
+print("List:", fruits) 
+
+# tuple
+coordinates = (10, 20)
+print("Tuple:", coordinates)  
+
+# dict (dictionary)
+person = {"name": "Sumbal", "role": "Code Queen"}
+print("Dictionary:", person)  
+
+# set
+unique_numbers = {1, 2, 3, 3}
+print("Set:", unique_numbers)  
+
+# NoneType
+value = None
+print("NoneType:", value)  
