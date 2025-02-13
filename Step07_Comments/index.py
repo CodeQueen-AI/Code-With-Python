d37a9237f22661f79f3bef9cf95b42e6ba167c4e
Sumbal Naz <Codeq209@gmail.com>
2025-02-13 14:26:49 +0500

    Add files via upload

---
+++ Step07_Comments/index.py
@@ -0,0 +1,10 @@
+#Single Line Comment
+#Hello World
+print("Hello, World!")  
+
+#Multi Line Comment
+"""
+Ye ek multi-line comment hai.
+Ye multiple lines tak ja sakta hai.
+"""
+print("Python is awesome!")
